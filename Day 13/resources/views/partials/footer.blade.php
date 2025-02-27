0635ebb60e5293f5991f1082f50e8b20ebbc70a4
Bakudapa <149034440+Bakudapa@users.noreply.github.com>
2025-02-27 10:48:57 +0700

    Commit day 13

---
+++ Day 13/resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer id="footer" class="footer mt-auto py-3 bg-dark text-white text-center">
+    <div class="container">
+        <p>© <span>Copyright</span> <strong class="px-1 sitename">Company</strong> <span>All Rights Reserved</span></p>
+        <div class="credits">
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> 
+            Distributed by <a href="https://themewagon.com">ThemeWagon</a>
+        </div>
+    </div>
+</footer>
---
+++ Day 13/resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+@section('title')
+    DASHBOARD
+@endsection
+@section('content')
+<h1>SELAMAT DATANG!</h1><br>
+<h2>Terimakasih telah bergabung di sanberbook. Social Media kita bersama!</h2>
+@endsection
+
+<!--
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>SELAMAT DATANG!</h1><br>
+        <h2>Terimakasih telah bergabung di sanberbook. Social Media kita bersama!</h2>
+    </body>
+        </html>
+!-->
